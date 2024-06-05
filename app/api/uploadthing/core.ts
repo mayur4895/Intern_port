ce11167bb48f31c5eb0d010dac13d4fd0166300d
mayur shinde <kssmsslss330@gmail.com>
2024-06-05 13:00:10 +0530

    "done uploadthing"

--- app/api/uploadthing/core.ts
+++ app/api/uploadthing/core.ts
@@ -1,26 +1,21 @@
+ 
+import { currentUser } from "@/lib/auth";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
  
 const f = createUploadthing();
+const handleAuth = async()=>{
+    const user = await currentUser();
+    console.log(user); 
+    
+    if(!user) throw new Error("Unauthorized");
+    return user;
+ }
  
-const auth = (req: Request) => ({ id: "fakeId" });   
-  
 export const ourFileRouter = {  
   imageUploader: f({ image: { maxFileSize: "4MB" } }) 
-    .middleware(async ({ req }) => { 
-      const user = await auth(req); 
-      if (!user) throw new UploadThingError("Unauthorized"); 
-      return { userId: user.id };
-    })
-    .onUploadComplete(async ({ metadata, file }) => { 
-      console.log("Upload complete for userId:", metadata.userId);
- 
-      console.log("file url", file.url);
-
-      
- 
-      return { uploadedBy: metadata.userId };
-    }),
+  .middleware(()=> handleAuth())
+  .onUploadComplete(()=>{})
 } satisfies FileRouter;
  
 export type OurFileRouter = typeof ourFileRouter;

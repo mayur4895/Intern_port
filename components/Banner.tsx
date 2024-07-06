39c7a82eaa0ed185dc04185184ba84e12bc72bf7
mayur shinde <kssmsslss330@gmail.com>
2024-07-06 22:50:40 +0530

    "done done"

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -5,9 +5,9 @@ import { Button } from './ui/button'
 const Banner = () => {
   return (
  
-  <div className='w-full h-screen m-auto flex   gap-4 justify-center items-center text-center'>
+  <div className='w-full h-auto m-auto flex  mt-8  gap-4 justify-center items-center text-center'>
  <div className='flex flex-col gap-4'>
- <h1 className='md:text-6xl  md:max-w-screen-md sm:max-w-screen-sm  m-auto  sm:2xl text-3xl font-bold  text-zinc-600'>Search,Apply & Get Your <span className='text-blue-500'>Dream</span> Job.</h1>
+ <h1 className='md:text-6xl  md:max-w-screen-md sm:max-w-screen-sm  m-auto  sm:3xl text-5xl font-bold  text-zinc-600'>Search,Apply & Get Your <span className='text-blue-500'>Dream</span> Job.</h1>
       <p className='md:text-sm text-xs'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias ad corrupti accusamus quis, sunt rem.</p>
       <div><Button>Browse Jobs</Button></div>
  </div>

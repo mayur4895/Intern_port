0089faed067bb77e73bfa06656f216cd7d433315
mayur shinde <101582238+mayur4895@users.noreply.github.com>
2024-08-05 10:59:45 +0530

    Update JobsSection.tsx

--- components/student/JobsSection.tsx
+++ components/student/JobsSection.tsx
@@ -14,6 +14,7 @@ const JobSection = () => {
                 return( 
 
                     <InternshipCard
+                        key={internship.id}
                     internship={internship}
                     />
                 )

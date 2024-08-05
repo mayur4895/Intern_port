198e9d1a8984abaa6d783a4e3fba241a3be1934d
mayur shinde <kssmsslss330@gmail.com>
2024-08-05 21:48:21 +0530

    "fixed bug"

--- components/hire-talent/DashboardHeader.tsx
+++ components/hire-talent/DashboardHeader.tsx
@@ -65,30 +65,7 @@ const DashboardHeader = () => {
  
     const pathname = usePathname();
     
-  return (
-//    <header className=' text-white  py-8 lg:px-18 pb-36 px-4 bg-gradient-to-b from-blue-700 to to-blue-500'> 
-//    <div className=' max-w-screen-2xl mx-auto'>
-//      <div className='mb-14 w-full flex items-center justify-between'>
- 
-//  <div className='flex items-end gap-2 '>
-//  <PiBuildingsFill size={30}/> Modern
-//  </div>
-//  <NavigationDashboard/>
-//  <div className=' hidden lg:block'>
-//  <UserButton/> 
-
-//  </div>
-//       </div>
-
-//     <div className=' flex flex-col gap-3 text-white'>
-//     <h3 className='text-2xl'>Welcome Back {currentUser?.name} 👋</h3>
-//      <h2 className=' mt-0 text-xl'> Company: {currentUser?.companyDetails?.name}</h2>
-//      <MarkupContent content={currentUser?.companyDetails?.description}  className={'text-white'}/>
- 
-//      <span className='flex items-center gap-2'><FaRegBuilding size={22}/> {currentUser?.companyDetails?.employees} Employees </span>
-//     </div>
-//    </div>
-//    </header>
+  return ( 
 <header className="sticky  py-2  top-0 z-30 flex h-14 items-center gap-4 border-b bg-background   sm:static sm:h-auto sm:border-0 sm:bg-transparent px-4">
 <Sheet>
   <SheetTrigger asChild>

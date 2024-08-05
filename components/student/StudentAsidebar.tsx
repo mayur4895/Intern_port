701cf2f8e003ba056eebd8133d620dcdfb5e16bc
mayur shinde <101582238+mayur4895@users.noreply.github.com>
2024-08-05 11:20:22 +0530

    Update StudentAsidebar.tsx

--- components/student/StudentAsidebar.tsx
+++ components/student/StudentAsidebar.tsx
@@ -10,6 +10,8 @@ import { CurrentUser } from '@/hooks/use-current-user'
 import { PiSuitcaseSimpleLight } from 'react-icons/pi'
 import { TbBrandGoogleHome } from 'react-icons/tb'
 import { CiHome } from 'react-icons/ci'
+import { signOut } from 'next-auth/react'
+
 const StudentAsidebar = () => {
  
   const  pathname  =  usePathname();

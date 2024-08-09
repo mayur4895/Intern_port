d4b5856b1fc89cb78bb8c9a15ada6f935a1fca8a
mayur shinde <kssmsslss330@gmail.com>
2024-08-10 00:17:54 +0530

    "done todays"

--- app/auth/(routes)/login/layout.tsx
+++ app/auth/(routes)/login/layout.tsx
@@ -18,7 +18,7 @@ const LoginPagelayout = ({children}:{children:React.ReactNode}) => {
     <div className=' w-full flex items-center md:h-screen h-auto   bg-blue-800/10 justify-center'> 
     
       <div className=' relative  shadow-md rounded-md'>
-      <Link href={"/"} className=" absolute right-5 top-5 hidden md:block cursor-pointer "><TfiClose size={22} className='text-gray-500'/></Link>
+      <Link href={"/"} className=" absolute right-5 top-5  cursor-pointer "><TfiClose size={22} className='text-gray-500'/></Link>
       <Tabs
           defaultValue={type == "employer" ? "employer" : "student"}
           className=" lg:h-[600px]  h-auto top-10   shadow-sm   bg-white rounded-md border w-full flex flex-col   items-center justify-center">

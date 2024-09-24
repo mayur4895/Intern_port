6db6d57189d8ae29aeda75eab2210c236757f81a
mayur shinde <kssmsslss330@gmail.com>
2024-09-24 10:30:03 +0530

    "bug fixed"

--- features/application/api/getapplicationsofPost.ts
+++ features/application/api/getapplicationsofPost.ts
@@ -1,28 +1,22 @@
 import { getApplicationsofPost } from "@/actions/hire-talent/getApplicationsofPost";
 import { useQuery } from "@tanstack/react-query";
-import axios from "axios";
 
 export const useGetApplicationofPost = (postId: string) => {
   return useQuery({
     queryKey: ['ApplicationsofPost', postId],
     queryFn: async () => {
-<<<<<<< HEAD
-      try {     
-        const result = await getApplicationsofPost(postId);  
-=======
       try {
-        const result = await getApplicationsofPost(postId); 
-    
->>>>>>> 4f65923a7d1ca3bd75068350a5fbd281650e454c
+        const result = await getApplicationsofPost(postId);
+
         if (result.error) {
           throw new Error(result.error);
         }
-   
+
         return result.data;
       } catch (error) {
         console.error('Error fetching post applications:', error);
-        throw error;  
+        throw error;
       }
-    },
+    }
   });
 };
